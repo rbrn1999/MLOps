fe8318aa3c3740a36776be72fc2b910b1fa5f16b
Ian Liu <ianliu@livemail.tw>
2023-04-14 22:19:04 +0800

    add headSha check

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, json
+from flask import Flask, request
 import subprocess
 
 app = Flask(__name__)
@@ -10,9 +10,24 @@ def hello():
 @app.route('/commit_webhook', methods=['POST'])
 def didCommit():
     data = request.json
-    app.logger.info(data)
+    app.logger.info(f'commit: {data["repo"]["headSha"]}')
+    cur_headSha = data["repo"]["headSha"]
+    prev_headSha = ""
+    try:
+        with open("commit.txt", "r") as f:
+            prev_headSha = f.read()
+    except IOError:
+        app.logger.warn("can't read 'commit.txt'")
+
+    if cur_headSha == prev_headSha:
+        app.logger.info("Not a new commit, will not proceed to training.")
+        return "Not a new commit, will not proceed to training."
+
+    with open("commit.txt", "w+") as f:
+        f.write(cur_headSha)
     subprocess.run(["python", "esgBERTv4.py"], cwd="/workspace/Step3")
+
     return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
